839e08399cf54f23dadbf67740bae6805d162f35
Manolo <manuelgmaroto@gmail.com>
2025-06-27 09:58:39 +0200

    Add cars get and put

--- express.js
+++ express.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const app = express();
 
+const cars = [
+    {
+        id: 0,
+        name: 'Seat',
+        model: 'Leon'
+    },
+    {
+        id:1,
+        name: 'Ford',
+        model: 'Explorer'
+    }
+]
+
 app.get('/', (req, res) => {
     res.send('<h1>Hola</h1>')
 });
+
+app.get('/coches', (req,res,next) => {
+    //console.log(req)
+    res.send(cars);
+})
+
+app.get('/coches/:id', (req,res,next) => {
+    const idCar = req.params.id;
+    if(idCar) {
+        res.send(cars[idCar].name);
+    } else {
+        res.status(404).send('Marca no encontrada');
+    }
+})
+
+app.put('/coches/:id', (req,res,next) => {
+    const idCar = req.params.id;
+    const newData = req.query;
+    console.log(idCar)
+    res.send(newData);
+})
+/* 
+app.post('/coches') */
 
 app.listen(8000, () => {
     console.log('Server listening on port 8000');
